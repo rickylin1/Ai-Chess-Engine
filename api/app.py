7525dac3db187f1df4b9eff226213009c5ed0a59
rickylin1 <pandanation33@gmail.com>
2024-06-08 15:37:59 -0700

    Attempt to fix vercel deployment

--- api/app.py
+++ api/app.py
@@ -106,5 +106,5 @@ def reset_game():
 
 
 
-if __name__ == '__main__':
-    app.run(host = '127.0.0.1', port = 8080, debug=True)
+# if __name__ == '__main__':
+#     app.run(host = '127.0.0.1', port = 8080, debug=True)
